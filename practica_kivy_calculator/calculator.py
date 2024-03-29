abcafde6fa71643323e4186fc376b77a232b278b
Manuel Velasco <manuelvelascodevs@gmail.com>
2024-03-28 23:12:41 -0300

    Se añade la funcionalidad de ingresar numero y borrarlos todos en la calculadora

--- practica_kivy_calculator/calculator.py
+++ practica_kivy_calculator/calculator.py
@@ -7,8 +7,35 @@ from kivy.uix.gridlayout import GridLayout
 
 
 class Calculator(App):
-    pass
 
+    label = ""
+
+    def presser(self, btn):
+        print(btn)
+        if btn == "CE":
+            self.aplicarCe()
+            return
+        if(btn in ["1","2","3","4","5","6","7","8","9","0"]):
+            print("El resultado es un numero " + btn)
+            self.aplicarNumero(btn)
+        else: print("El resultado no es un numero")
+        pass
+
+    def aplicarNumero(self, value):
+        print(len(self.label))
+        print(len(self.label)==0)
+        if(True):#self.label == "" and len(self.label)==0):
+            self.actualizarLabel(value)
+
+    def aplicarCe(self):
+        self.label = ""
+        self.root.ids.label.text = self.label
+
+    def actualizarLabel(self,value):
+        if(self.label == "0"):
+            return
+        self.label = self.label + value
+        self.root.ids.label.text =self.label
 
 if __name__ == '__main__':
     Calculator().run()
