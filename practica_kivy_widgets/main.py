8d06a8a168e6d980cfec07d66aa04c0dad98729d
Manuel Velasco <manuelvelascodevs@gmail.com>
2024-04-24 16:16:09 -0300

    se añaden los archivos para los widgets en kivy

---
+++ practica_kivy_widgets/main.py
@@ -0,0 +1,20 @@
+import kivy
+
+from kivy.app import App
+
+class Widgets(App):
+    pass
+
+if __name__ == '__main__':
+    Widgets().run()
+
+
+
+
+# Screen:
+#         name: "Pantalla 2"
+#         BoxLayout:
+#             orientation: "vertical"
+#             canvas:
+#                 Color:
+#                     rgb: [0.5,0.0]
