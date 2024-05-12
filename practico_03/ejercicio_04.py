297b7c156a90c98c55d4f0f7b6d61a581facc287
joaquin <betesjoaquin@gmail.com>
2024-05-12 16:48:15 -0300

    ejs 4 y 5

--- practico_03/ejercicio_04.py
+++ practico_03/ejercicio_04.py
@@ -17,8 +17,12 @@ class Perro(Animal):
     sobrecargar el método descripción para que devuelva:
     "Soy un perro y" + método descripción del padre
     """
-    # Completar
+    def __init__(self, edad: int = 0, raza: str = ""):
+        super().__init__(edad=edad)
+        self.raza = raza
 
+    def descripcion(self) -> str:
+        return f"Soy un perro y {super().descripcion().lower()}"
 
 # NO MODIFICAR - INICIO
 terrier = Perro(edad=8, raza="Yorkshire Terrier")
@@ -39,19 +43,21 @@ from dataclasses import dataclass
 
 @dataclass
 class Animal:
-    pass # Completar
-
+    edad: int = 0
+    def descripcion(self) -> str:
+        return f"Tengo {self.edad} años"
 
 @dataclass
 class Perro(Animal):
-    pass # Completar
+    raza: str=""  
+    def descripcion(self) -> str:
+        return f"Soy un perro y {super().descripcion().lower()}"
 
 
 # NO MODIFICAR - INICIO
 terrier = Perro(edad=8, raza="Yorkshire Terrier")
 cachorro = Perro(edad=1)
 dogo = Perro(raza="Dogo")
-
 assert Animal(10).descripcion() == "Tengo 10 años"
 assert terrier.descripcion() == "Soy un perro y tengo 8 años"
 assert dogo.descripcion() == "Soy un perro y tengo 0 años"
