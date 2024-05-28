d3800762547cfc33f1d655f4dd06682d2e89d0b7
7Rena7 <renacorbe@gmail.com>
2024-05-28 17:40:43 -0300

    Avances con la cámara y kivy

--- practica_opencv/photo.py
+++ practica_opencv/photo.py
@@ -1,6 +1,11 @@
 from kivy.app import App
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.image import Image
 import time
-
+import cv2
 
 class Photo(App):
     def capture(self):
@@ -8,6 +13,22 @@ class Photo(App):
         timestr = time.strftime("%Y%m%d_%H%M%S")
         camera.export_to_png("images/IMG_{}.png".format(timestr))
         print("Foto tomada", "images/IMG_{}.png".format(timestr))
+
+    #     layout = BoxLayout(orientation='vertical')
+    #     self.image = Image()
+    #     layout.add_widget(self.image)
+    #     layout.add_widget(Button(text="Tomar foto"))
+    #     self.capture = cv2.VideoCapture(0)
+    #     Clock.schedule_interval(self.load_video, 1.0/60.0)
+    #     return layout
+    #
+    # def load_video(self, *args):
+    #     ret, frame = self.capture.read()
+    #     self.image_frame = frame
+    #     buffer = cv2.flip(frame, 0).tostring()
+    #     texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+    #     texture.blit_buffer(buffer, colorfmt='bgr', bufferfmt='ubyte')
+    #     self.image.texture = texture
 
 
 if __name__ == '__main__':
